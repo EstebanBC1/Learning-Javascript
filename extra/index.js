45e3c2f58331e202ff99bec05e6a6d44dd3079a1
Esteban <estebanbcastro@gmail.com>
2023-08-03 20:18:46 -0700

    New edits

---
+++ extra/index.js
@@ -0,0 +1,20 @@
+// var name = []; 
+// let firstName = nameEntry.firstName; 
+// let lastName = nameEntry.lastName;
+// function nameEntry(){
+//     firstName = prompt("Please enter your First Name."); 
+//     lastName = prompt("Please enter your Last Name.");
+// }
+// nameEntry(); 
+// alert(`Thank you for your submission ${firstName} ${lastName}`);
+// DEFINE YOUR FUNCTION BELOW:
+// DEFINE YOUR FUNCTION BELOW:
+
+function gotThem(num)
+{ 
+    function getThem()
+    { 
+        return console.log(`Hello ${num}`);
+    }
+   getThem();
+}
---
+++ javascriptPractice/javaRun.js
@@ -0,0 +1,10 @@
+const nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+// const primeNums = nums.forEach(function(num){
+//     console.log(`You are # ${num} in line`);
+// // })
+// const primeNums = nums.forEach ( num => {
+//     console.log(`hello`); 
+// })
+
+console.log(1 + "2" -1); 
